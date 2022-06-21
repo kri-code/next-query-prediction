dc81dd05e81f762857c7e21ad4b34f5dbe0bfdc1
kri-code <56919961+kri-code@users.noreply.github.com>
2022-06-21 20:01:48 +0200

    Add files via upload

---
+++ encode and decode/data.py
@@ -0,0 +1,24 @@
+import decode_dict as d
+
+input_data = [d.query_dict["1a"], d.query_dict["2a"], d.query_dict["3a"], d.query_dict["4a"], d.query_dict["5a"], d.query_dict["6a"], d.query_dict["7a"], d.query_dict["8a"], d.query_dict["9a"], d.query_dict["10a"],\
+    d.query_dict["11a"], d.query_dict["12a"], d.query_dict["13a"], d.query_dict["14a"], d.query_dict["15a"], d.query_dict["16a"], d.query_dict["17a"], d.query_dict["18a"], d.query_dict["19a"], d.query_dict["20a"],\
+    d.query_dict["21a"], d.query_dict["22a"], d.query_dict["23a"], d.query_dict["24a"], d.query_dict["25a"], d.query_dict["26a"], d.query_dict["27a"], d.query_dict["28a"], d.query_dict["29a"], d.query_dict["30a"],\
+    d.query_dict["31a"], d.query_dict["32a"]]
+
+
+target_data = [d.query_dict["2a"], d.query_dict["3a"], d.query_dict["4a"], d.query_dict["5a"], d.query_dict["6a"], d.query_dict["7a"], d.query_dict["8a"], d.query_dict["9a"], d.query_dict["10a"],\
+    d.query_dict["11a"], d.query_dict["12a"], d.query_dict["13a"], d.query_dict["14a"], d.query_dict["15a"], d.query_dict["16a"], d.query_dict["17a"], d.query_dict["18a"], d.query_dict["19a"], d.query_dict["20a"],\
+    d.query_dict["21a"], d.query_dict["22a"], d.query_dict["23a"], d.query_dict["24a"], d.query_dict["25a"], d.query_dict["26a"], d.query_dict["27a"], d.query_dict["28a"], d.query_dict["29a"], d.query_dict["30a"],\
+    d.query_dict["31a"], d.query_dict["32a"], d.query_dict["33a"]]
+
+data = [[d.query_dict["1a"], d.query_dict["2a"]], [d.query_dict["2a"], d.query_dict["3a"]], [d.query_dict["3a"], d.query_dict["4a"]], [d.query_dict["4a"], d.query_dict["5a"]],\
+    [d.query_dict["5a"], d.query_dict["6a"]], [d.query_dict["6a"], d.query_dict["7a"]], [d.query_dict["7a"], d.query_dict["8a"]],\
+    [d.query_dict["8a"], d.query_dict["9a"]], [d.query_dict["9a"], d.query_dict["10a"]], [d.query_dict["10a"], d.query_dict["11a"]],\
+    [d.query_dict["11a"], d.query_dict["12a"]], [d.query_dict["12a"], d.query_dict["13a"]], [d.query_dict["13a"], d.query_dict["14a"]],\
+    [d.query_dict["14a"], d.query_dict["15a"]], [d.query_dict["15a"], d.query_dict["16a"]], [d.query_dict["16a"], d.query_dict["17a"]],\
+    [d.query_dict["17a"], d.query_dict["18a"]], [d.query_dict["18a"], d.query_dict["19a"]], [d.query_dict["19a"], d.query_dict["20a"]], \
+    [d.query_dict["20a"], d.query_dict["21a"]], [d.query_dict["21a"], d.query_dict["22a"]], [d.query_dict["22a"], d.query_dict["23a"]], \
+    [d.query_dict["23a"], d.query_dict["24a"]], [d.query_dict["24a"], d.query_dict["25a"]], [d.query_dict["25a"], d.query_dict["26a"]],\
+    [d.query_dict["26a"], d.query_dict["27a"]], [d.query_dict["27a"], d.query_dict["28a"]], [d.query_dict["28a"], d.query_dict["29a"]],\
+    [d.query_dict["29a"], d.query_dict["30a"]], [d.query_dict["30a"], d.query_dict["31a"]], [d.query_dict["31a"], d.query_dict["32a"]],\
+    [d.query_dict["32a"], d.query_dict["33a"]]]
